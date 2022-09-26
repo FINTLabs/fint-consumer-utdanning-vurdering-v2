89e0ed85c320fd69aecaf02c55351f9fc6719534
Hknots <Hknots99@gmail.com>
2022-09-26 10:37:52 +0200

    Added service & Correct config

--- src/main/java/no/fintlabs/consumer/elevfravar/ElevfravarKafkaConsumer.java
+++ src/main/java/no/fintlabs/consumer/elevfravar/ElevfravarKafkaConsumer.java
@@ -6,16 +6,18 @@ import no.fintlabs.core.consumer.shared.resource.kafka.EntityKafkaConsumer;
 import no.fintlabs.kafka.common.ListenerBeanRegistrationService;
 import no.fintlabs.kafka.entity.EntityConsumerFactoryService;
 import no.fintlabs.kafka.entity.topic.EntityTopicService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ElevfravarKafkaConsumer extends EntityKafkaConsumer<ElevfravarResource> {
     public ElevfravarKafkaConsumer(
             EntityConsumerFactoryService entityConsumerFactoryService,
             ListenerBeanRegistrationService listenerBeanRegistrationService,
             EntityTopicService entityTopicService,
-            ConsumerConfig consumerConfig) {
+            ElevfravarConfig elevfravarConfig) {
         super(entityConsumerFactoryService,
                 listenerBeanRegistrationService,
                 entityTopicService,
-                consumerConfig);
+                elevfravarConfig);
     }
 }
